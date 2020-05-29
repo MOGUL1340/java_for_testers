ccbe6c8f4ce10a7e3840065648765ab3b5aeae59
Mogul1340 <antonioua1340@gmail.com>
2020-05-29 12:18:49 +0300

    Optimisation for method type in HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("TestGroup", "TestHeader", "TestFooter"));
+    app.getGroupHelper().fillGroupForm(new GroupData("TestGroup", null, null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnGroupPage();
     app.logout();
