ec01a149c112f5a796c97ad87058a32f36a01b1e
Mogul1340 <antonioua1340@gmail.com>
2020-06-11 22:02:48 +0300

    Added verification to ContactModification + ContactDeletion tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,8 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
   @Test
@@ -15,6 +18,7 @@ public class ContactDeletionTests extends TestBase {
     }
     // Check contact availability
     app.getNavigationHelper().returnToHomepage();
+    List<ContactData> before = app.getContactHelper().getContactList();
     if (! app.getContactHelper().isContactPresent()) {
       app.getContactHelper().createContact(new ContactData("Name", "MiddleName",
               "LastName", "Nickname", "Company",
@@ -27,5 +31,10 @@ public class ContactDeletionTests extends TestBase {
     app.getContactHelper().submitContactDeletion();
     app.getContactHelper().acceptAlert();
     app.getNavigationHelper().returnHomeAfterDeletion();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,8 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
@@ -24,14 +28,25 @@ public class ContactModificationTests extends TestBase {
               "TestGroup", "TestDrive 123", "123", "test notes"), true);
       app.getNavigationHelper().returnToHomepage();
     }
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().initFirstContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("ModifiedName", "ModifiedMiddleName",
-            "ModifiedLastName", "Nickname", "Company",
+    ContactData contact = new ContactData("Name", "MiddleName",
+            "LastName", "Nickname", "Company",
             "+0123456789", "test@mail.com", "testpage.com",
             "1", "December", "1980",
-            null, "TestDrive 123", "123", "test notes"), false);
+            "TestGroup", "TestDrive 123", "123", "test notes");
+    app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().submitContactUpdate();
     app.getNavigationHelper().returnToHomepage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(contact);
+    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
 
