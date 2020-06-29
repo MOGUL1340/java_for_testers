d8151815e6c02c69a4f056088ebd8bf4f8054cf7
Mogul1340 <antonioua1340@gmail.com>
2020-06-29 12:45:44 +0300

    HW 12. Implement data provider usage in ContactCreation test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,18 +1,42 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTests extends TestBase {
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType());
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
 
   @BeforeMethod
   public void ensurePrecondition() {
@@ -23,14 +47,13 @@ public class ContactCreationTests extends TestBase {
     }
   }
 
-  @Test
-  public void testContactCreationTests() throws Exception {
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreationTests(ContactData contact) throws Exception {
 
     app.goTo().homePage();
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().withFirstName("FirstName").withLastName("LastName").withPhoto(photo).withGroup("TestGroup");
-    app.contact().createContact(contact, true);
+    app.contact().createContact(contact.withPhoto(photo).withGroup("TestGroup"), true);
     app.goTo().homePage();
     Contacts after = app.contact().all();
     assertEquals(after.size(), before.size() + 1);
